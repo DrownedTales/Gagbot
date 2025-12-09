ad053e6064836af199024638290d31281f1af577
Enraa <enraaaery@gmail.com>
2025-12-09 00:19:48 -0800

    Update stuff.js

--- gags/stuff.js
+++ gags/stuff.js
@@ -2,7 +2,7 @@ const garbleText = (text) => {
     let newtextparts = text.split(" ");
     let outtext = '';
     for (let i = 0; i < newtextparts.length; i++) {
-        let randomlength = newtextparts[i] + 3 - (6 * Math.random())
+        let randomlength = newtextparts[i].length + 3 - (6 * Math.random())
         for (let t = 0; t < randomlength - 2; t++) {
             if (t == 0) {
                 outtext = `${outtext}M`
