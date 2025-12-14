ce2aae90ac2e8fdd895df07c1bf51bd791424d04
dollliandra <dollliandra@gmail.com>
2025-12-13 19:47:23 -0500

    Beginning charmaps

---
+++ gags/ball/ballCharMap.js
@@ -0,0 +1,42 @@
+/************************************************
+ * Character Maps for Ball Gag Intensity Slider *
+ * 
+ * ~DOLL-14
+ ************************************************/
+
+
+const ballGagCharMaps = [ballCharMap_Intensity3];
+
+
+const ballCharMap_Intensity3 = new Map([
+    ['a',"h"],
+    ['b',"b"],
+    ['c',"g"],
+    ['d',"d"],
+    ['e',"m"],
+    ['f',"f"],
+    ['g',"g"],
+    ['h',"h"],
+    ['i',"hm"],       // Unique two-char case
+    ['j',"j"],
+    ['k',"g"],
+    ['l',"l"],
+    ['m',"m"],
+    ['n',"n"],
+    ['o',"h"],
+    ['p',"p"],
+    ['q',"g"],
+    ['r',"r"],
+    ['s',"f"],
+    ['t',"t"],
+    ['u',"h"],
+    ['v',"v"],
+    ['w',"w"],
+    ['x',"m"],
+    ['y',"n"],
+    ['z',"m"],
+]);
+
+
+// Export the array for use in other functions.
+exports.ballGagCharMaps = ballGagCharMaps;
