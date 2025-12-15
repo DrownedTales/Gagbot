12a798bc655e6f69808a51ac072531e2f543b563
Enraa <enraaaery@gmail.com>
2025-12-14 17:46:35 -0800

    bast.js

--- commands/unmitten.js
+++ commands/unmitten.js
@@ -11,7 +11,7 @@ module.exports = {
 			.setDescription('Who to free from their mittens')
 		),
     async execute(interaction) {
-		let mitteneduser = interaction.options.getUser('user')
+		let mitteneduser = interaction.options.getUser('user') ? interaction.options.getUser('user') : interaction.user
 		if (getHeavy(interaction.user.id)) {
 			if (interaction.options.getUser('user') == interaction.user) {
             	interaction.reply(`${interaction.user} wriggles their hands in their ${getHeavy(interaction.user.id).type}, but can't get good leverage to take their mittens off!`)
@@ -20,15 +20,15 @@ module.exports = {
 				interaction.reply(`${interaction.user} uses their nose because of their ${getHeavy(interaction.user.id).type}, but can't help ${interaction.options.getUser('user')} out of their mittens!`)
 			}
         }
-        else if (interaction.options.getUser('user') == interaction.user) {
+        else if (mitteneduser == interaction.user) {
             interaction.reply(`${interaction.user} tries to take their mittens off, but can't get a good grip on the straps!`)
         }
 		else if (getMitten(mitteneduser)) {
             deleteMitten(mitteneduser)
-            interaction.reply(`${interaction.user} takes off ${interaction.options.getUser('user')}'s mittens so they can take off their gag!`)
+            interaction.reply(`${interaction.user} takes off ${mitteneduser}'s mittens so they can take off their gag!`)
         }
 		else {
-			interaction.reply({ content: `${interaction.options.getUser('user')} is not wearing mittens!`, flags: MessageFlags.Ephemeral })
+			interaction.reply({ content: `${mitteneduser} is not wearing mittens!`, flags: MessageFlags.Ephemeral })
         }
     }
 }
